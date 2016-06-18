6012eeba5e5b10ef17a202a69ce59bd7b5cf4595
Dmitry Malkovich <dima.malkovich@gmail.com>
2016-06-18 21:20:56 +0300

    feat: Java library for getting jokes

--- app/src/main/java/com/dmitrymalkovich/android/builditbigger/MainActivity.java
+++ app/src/main/java/com/dmitrymalkovich/android/builditbigger/MainActivity.java
@@ -2,8 +2,11 @@ package com.dmitrymalkovich.android.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
+
+import com.dmitrymalkovich.java.LaughFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast toast = Toast.makeText(this, LaughFactory.tellMeJoke(), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
---
+++ jokes-library/src/main/java/com/dmitrymalkovich/java/LaughFactory.java
@@ -0,0 +1,9 @@
+package com.dmitrymalkovich.java;
+
+public class LaughFactory {
+    public static String tellMeJoke() {
+        return "Two bytes meet. The first byte asks, “Are you ill?”\n" +
+                "\n" +
+                "The second byte replies, “No, just feeling a bit off.”";
+    }
+}
