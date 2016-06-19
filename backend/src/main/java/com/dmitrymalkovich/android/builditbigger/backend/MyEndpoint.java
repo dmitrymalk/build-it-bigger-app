9b89db38ebac09b2266b57a51f7a64efb16467a2
Dmitry Malkovich <dima.malkovich@gmail.com>
2016-06-19 21:07:09 +0300

    feat: License added

--- backend/src/main/java/com/dmitrymalkovich/android/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/dmitrymalkovich/android/builditbigger/backend/MyEndpoint.java
@@ -1,9 +1,18 @@
 /*
-   For step-by-step instructions on connecting your Android application to this backend module,
-   see "App Engine Java Endpoints Module" template documentation at
-   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
-*/
-
+ * Copyright 2016.  Dmitry Malkovich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dmitrymalkovich.android.builditbigger.backend;
 
 import com.dmitrymalkovich.java.LaughFactory;
