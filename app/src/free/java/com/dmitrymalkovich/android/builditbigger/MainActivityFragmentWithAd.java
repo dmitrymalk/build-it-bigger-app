3046103534140f816127521368a67e66cbf3129e
Dmitry Malkovich <dima.malkovich@gmail.com>
2016-06-21 00:34:27 +0300

    feat: Add a Paid Flavor
    Paid version without any Ads

---
+++ app/src/free/java/com/dmitrymalkovich/android/builditbigger/MainActivityFragmentWithAd.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016.  Dmitry Malkovich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dmitrymalkovich.android.builditbigger;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivityFragmentWithAd extends MainActivityFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        AdView mAdView = ButterKnife.findById(root, R.id.ad_view);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
+    }
+}
--- app/src/main/java/com/dmitrymalkovich/android/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/dmitrymalkovich/android/builditbigger/MainActivityFragment.java
@@ -16,16 +16,14 @@
 package com.dmitrymalkovich.android.builditbigger;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.dmitrymalkovich.android.jokes.viewer.JokeViewerActivity;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -59,15 +57,6 @@ public class MainActivityFragment extends Fragment {
                 }).execute();
             }
         });
-
-        AdView mAdView = ButterKnife.findById(root, R.id.ad_view);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         return root;
     }
 
